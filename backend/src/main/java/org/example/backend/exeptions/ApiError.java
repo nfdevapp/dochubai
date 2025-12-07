8075033312f06525f1fa604b00472c653e330b05
Ivan <>
2025-12-07 14:06:02 +0100

    - add comments

--- backend/src/main/java/org/example/backend/exeptions/ApiError.java
+++ backend/src/main/java/org/example/backend/exeptions/ApiError.java
@@ -2,7 +2,7 @@ package org.example.backend.exeptions;
 
 
 /**
- * Wird message und status geschickt als JSON-Fehlerobjekt
+ * Wird message und status geschickt als JSON-Fehlerobjekt: status + message
  */
 public record ApiError(
         String message,
